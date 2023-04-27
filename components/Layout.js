f55704367029ef08aaf95ba1642f930142d8cada
Konstantyn <k.buhantsev@gmail.com>
2023-04-27 12:49:51 +0300

    layout & navigation

---
+++ components/Layout.js
@@ -0,0 +1,35 @@
+import Nav from "@/components/Nav";
+import { signIn, useSession } from "next-auth/react";
+
+export default function Layout({ children }) {
+  const { data: session } = useSession();
+
+  if (!session) {
+    return (
+      <div className="bg-slate-800 w-screen h-screen flex items-center">
+        <div className="text-center w-full text-white"></div>
+        <div>
+          Not signed in <br />
+          <button
+            className="text-black bg-white p-2 px-4 rounded-lg hover:bg-gray-300
+              transition-colors"
+            onClick={() => {
+              signIn("google");
+            }}
+          >
+            Sign in with Google
+          </button>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="bg-blue-900 min-h-screen flex ">
+      <Nav />
+      <div className="bg-white flex-grow mt-2 mr-2 mb-2 rounded-lg p-4">
+        {children}
+      </div>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,11 @@
-import Nav from "@/components/Nav";
-import { signIn, useSession } from "next-auth/react";
+import Layout from "@/components/Layout";
+import { useSession } from "next-auth/react";
 
 export default function Home() {
   const { data: session } = useSession();
-
-  if (!session) {
-    return (
-      <div className="bg-slate-800 w-screen h-screen flex items-center">
-        <div className="text-center w-full text-white"></div>
-        <div>
-          Not signed in <br />
-          <button
-            className="text-black bg-white p-2 px-4 rounded-lg hover:bg-gray-300
-              transition-colors"
-            onClick={() => {
-              signIn("google");
-            }}
-          >
-            Sign in with Google
-          </button>
-        </div>
-      </div>
-    );
-  }
-
   return (
-    <div className="bg-blue-900 min-h-screen">
-      <div>Signed in as {session.user.email}</div>
-      <Nav />
-    </div>
+    <Layout>
+      <div className="text-blue-900">Hello, {session?.user.name} </div>
+    </Layout>
   );
 }

7fb380ceb9bd4d8fee340455f8039133c0f3476c
Konstantyn <k.buhantsev@gmail.com>
2023-04-27 11:59:59 +0300

    navigation

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,3 +1,5 @@
+import clientPromise from "@/lib/mongodb";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
@@ -10,6 +12,7 @@ export const authOptions = {
     }),
     // ...add more providers here
   ],
+  adapter: MongoDBAdapter(clientPromise),
 };
 
 export default NextAuth(authOptions);
--- pages/index.js
+++ pages/index.js
@@ -1,51 +1,33 @@
-import { signIn, signOut, useSession } from "next-auth/react";
+import Nav from "@/components/Nav";
+import { signIn, useSession } from "next-auth/react";
 
 export default function Home() {
   const { data: session } = useSession();
 
-  return (
-    <div className="bg-blue-700 w-screen h-screen flex items-center">
-      <div className="text-center w-full">
-        {session ? (
-          <div>
-            Signed in as {session.user.email}
-            <br />
-            <button
-              className="bg-white p-2 px-4 rounded-lg hover:bg-gray-300"
-              onClick={() => signOut("google")}
-            >
-              Sign out
-            </button>
-          </div>
-        ) : (
-          <div>
-            Not signed in <br />
-            <button
-              className="bg-white p-2 px-4 rounded-lg hover:bg-gray-300"
-              onClick={() => {
-                signIn("google");
-              }}
-            >
-              Sign in with Google
-            </button>
-          </div>
-        )}
+  if (!session) {
+    return (
+      <div className="bg-slate-800 w-screen h-screen flex items-center">
+        <div className="text-center w-full text-white"></div>
+        <div>
+          Not signed in <br />
+          <button
+            className="text-black bg-white p-2 px-4 rounded-lg hover:bg-gray-300
+              transition-colors"
+            onClick={() => {
+              signIn("google");
+            }}
+          >
+            Sign in with Google
+          </button>
+        </div>
       </div>
+    );
+  }
+
+  return (
+    <div className="bg-blue-900 min-h-screen">
+      <div>Signed in as {session.user.email}</div>
+      <Nav />
     </div>
   );
-
-  // if (session) {
-  //   return (
-  //     <>
-  //       Signed in as {session.user.email} <br />
-  //       <button onClick={() => signOut()}>Sign out</button>
-  //     </>
-  //   );
-  // }
-  // return (
-  //   <>
-  //     Not signed in <br />
-  //     <button onClick={() => signIn()}>Sign in</button>
-  //   </>
-  // );
 }

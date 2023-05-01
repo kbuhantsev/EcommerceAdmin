d2bafe6bb0a404a3d8842c445f5d982acc2b09d4
Konstantyn <k.buhantsev@gmail.com>
2023-05-01 23:32:45 +0300

    Spinner and images saving

---
+++ components/FadeSpinner.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { FadeLoader } from "react-spinners";
+
+const FadeSpinner = () => {
+  return <FadeLoader color={"#1E3A8A"} />;
+};
+
+export default FadeSpinner;
